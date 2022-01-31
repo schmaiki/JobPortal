9d27ad23ad6d9abde56f5b9d2fe4f90a8e08ceb5
Schmaiki <maikpeters1511@gmail.com>
2022-01-31 13:47:34 +0100

    Index angepasst und Js eingefügt

--- JobPortal/GlobalUsing.cs
+++ JobPortal/GlobalUsing.cs
@@ -4,5 +4,6 @@ global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Identity;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity;
 
 namespace JobPortal;
